50b123f6b1b9dad75849ac4c857e5fb4eff3937b
Bilal Ahmed <pk92love@gmail.com>
2021-11-21 23:39:58 +0500

    First javascript to git n destructuring object passed as a parameter for a funciton

---
+++ js.js
@@ -0,0 +1,21 @@
+const user = {
+    id: 42,
+    displayName: 'jdoe',
+    fullName: {
+      firstName: 'John',
+      lastName: 'Doe'
+    }
+  };
+  
+  function userId({id}) {
+    return id;
+  }
+  
+  function whois({displayName, fullName: {firstName: name}}) {
+    return `${displayName} is ${name}`;
+  }
+  
+  document.getElementById('root').innerHTML = userId(user)
+  document.getElementById('root2').innerHTML = whois(user)
+//   console.log(userId(user)); // 42
+//   console.log(whois(user));  // "jdoe is John"
