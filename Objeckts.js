271b4573746af2561d9a9cf2bede3ab9738aacf2
Bilal Ahmed <pk92love@gmail.com>
2021-11-22 14:27:20 +0500

    new Brach for objects is created

--- Objeckts.js
+++ Objeckts.js
@@ -1,23 +1,24 @@
 
 const user = {
-    id: 42,
-    displayName: 'jdoe',
-    fullName: {
-      firstName: 'John',
-      lastName: 'Doe'
-    }
-  };
-  
-  function userId({id}) {
-    return id;
+  id: 42,
+  displayName: 'jdoe',
+  fullName: {
+    firstName: 'John',
+    lastName: 'Doe'
   }
-  
-  function whois({displayName, fullName: {firstName: name}}) {
-    return `${displayName} is ${name}`;
-  }
-  
-  document.getElementById('root').innerHTML = userId(user)
-  document.getElementById('root2').innerHTML = whois(user)
+};
+
+function userId({ id }) {
+  return id;
+}
+
+function whois({ displayName, fullName }) {
+  let { firstName: name } = fullName
+  return `${displayName} is ${name}`;
+}
+
+document.getElementById('root').innerHTML = userId(user)
+document.getElementById('root2').innerHTML = whois(user)
 //   console.log(userId(user)); // 42
 //   console.log(whois(user));  // "jdoe is John"
 
